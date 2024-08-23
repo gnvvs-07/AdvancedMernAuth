dd912f161d0ccfcf113ca0738fc04e0dca137319
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-08-23 21:54:42 +0530

    login complete backend - success

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -85,7 +85,49 @@ export const verifyEmail = async (req, res) => {
     res.status(500).json({ success: false, message: "Failed to verify email" });
   }
 };
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  // logic: find the user by email
+  const { email, password } = req.body;
+  try {
+    // find the user in db
+    const user = await User.findOne({ email });
+    // if user doesnot exists
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User doesnot exist" });
+    }
+    // password verification
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    // if incorrect password
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({
+          success: false,
+          message: "Incorrect password and email combination",
+        });
+    }
+    // generate the token
+    generateTokenAndSetCookie(res, user._id);
+    // update user last login time
+    user.lastLogin = new Date();
+    await user.save();
+    // sending the response
+    res.status(200).json({
+      success: true,
+      message: "Login success",
+      user: {
+        ...user._doc,
+        // ^🕳️^ the password is set to undefined in the response not in the database
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error in loggin in");
+    throw new Error("Error while user login");
+  }
+};
 export const logout = async (req, res) => {
   // logic: remove the token from the user
   res.clearCookie("token");
