bd1e6efd292500c6edb5becacbfc6af01dd7a738
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-08-24 15:49:20 +0530

    verify email UI complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
 export default function App() {
   return (
     <div
@@ -34,6 +35,7 @@ export default function App() {
         <Route path="/" element={"Home"} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-email" element={<EmailVerificationPage/>}/>
       </Routes>
     </div>
   );
