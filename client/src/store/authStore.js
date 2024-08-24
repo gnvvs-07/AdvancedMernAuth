147fd872b75a784eb75349f7b2634da0479f7359
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-08-24 19:09:44 +0530

    email verification code for sign up complete

---
+++ client/src/store/authStore.js
@@ -0,0 +1,36 @@
+// zustand is used for statemanagement
+import { create } from "zustand";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+// Authentication store
+export const useAuthStore = create((set) => ({
+  // initial state
+  user: null,
+  isAuthentication: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: true,
+  // actions
+  signup: async (email, password, name) => {
+    set({ isLoading: true, error: null });
+    try {
+      // response
+      const response = await axios.post("/api/auth/signup", {
+        email,
+        password,
+        name,
+      });
+      set({
+        user: response.data.user,
+        isAuthentication: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error in signin up",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
+}));
